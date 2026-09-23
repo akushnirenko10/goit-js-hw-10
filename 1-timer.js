2454493826a77a78e897ba8bd2ad1d31398c37ba
akushnirenko10 <97456217+akushnirenko10@users.noreply.github.com>
2026-09-23 19:02:18 +0000

    Deploying to gh-pages from @ akushnirenko10/goit-js-hw-10@e2d88935b6e905566bb88489582963be9bc83786 🚀

---
+++ 1-timer.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-B5Qt9EMX.js";/* empty css                      */import{f as t}from"./assets/vendor-EyZmBGcZ.js";const o={datetimePicker:document.querySelector("#datetime-picker")},r={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(e){console.log(e[0])}};t(o.datetimePicker,r);
+//# sourceMappingURL=1-timer.js.map
