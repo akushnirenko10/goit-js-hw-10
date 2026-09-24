e7d4ce4c674dc59d96b9d31df6623632fa4e87f5
Andrii Kushnirenko <97456217+akushnirenko10@users.noreply.github.com>
2026-09-24 13:56:26 +0200

    Add 1 task

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,8 +1,14 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 import { refs } from './refs';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
-let userSelectedDate;
+let userSelectedDate = false;
+let TIMER_ID;
+const SET_INTERVAL_DELAY = 1000;
+
+refs.startBtn.disabled = true;
 
 const options = {
   enableTime: true,
@@ -12,6 +18,70 @@ const options = {
   onClose(selectedDates) {
     console.log(selectedDates[0]);
   },
+  onChange(selectedDates, instance) {
+    userSelectedDate = false;
+    if (Date.now() < selectedDates[0].getTime()) {
+      userSelectedDate = selectedDates[0];
+    }
+    if (!userSelectedDate) {
+      iziToast.error({
+        title: 'Error',
+        message: 'Please choose a date in the future',
+        position: 'topRight',
+      });
+    }
+
+    refs.startBtn.disabled = !userSelectedDate;
+  },
 };
 
 const fp = flatpickr(refs.datetimePicker, options);
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+
+function onStartBtnClick() {
+  TIMER_ID = setInterval(() => {
+    const timeDiff = userSelectedDate - Date.now();
+
+    refs.startBtn.disabled = timeDiff > 1000;
+    refs.datetimePicker.disabled = timeDiff > 1000;
+
+    const date = convertMs(timeDiff);
+
+    updateDateFields(date);
+
+    if (timeDiff < 1000) {
+      clearInterval(TIMER_ID);
+    }
+  }, SET_INTERVAL_DELAY);
+}
+
+function updateDateFields({ days, hours, minutes, seconds }) {
+  refs.daysField.textContent = addLeadingZero(days);
+  refs.hoursField.textContent = addLeadingZero(hours);
+  refs.minutesField.textContent = addLeadingZero(minutes);
+  refs.secondsField.textContent = addLeadingZero(seconds);
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,3 +1,8 @@
 export const refs = {
   datetimePicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('[data-start]'),
+  daysField: document.querySelector('[data-days]'),
+  hoursField: document.querySelector('[data-hours]'),
+  minutesField: document.querySelector('[data-minutes]'),
+  secondsField: document.querySelector('[data-seconds]'),
 };
